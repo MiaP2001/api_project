efab0c4a1b3e6f39f8d01a88af72bba900b7a6aa
MiaP2001 <petroviciutea@gmail.com>
2025-01-17 09:58:25 +0200

    Bug fixes

--- posts.js
+++ posts.js
@@ -3,20 +3,14 @@ const postsList = document.getElementById("posts-list");
 async function fetchPosts() {
   try {
     const postsResponse = await fetch(
-      "https://jsonplaceholder.typicode.com/posts"
+      "https://jsonplaceholder.typicode.com/posts?_expand=user&_embed=comments"
     );
     const posts = await postsResponse.json();
+    console.log(posts);
 
     for (const post of posts) {
-      const userResponse = await fetch(
-        `https://jsonplaceholder.typicode.com/users/${post.userId}`
-      );
-      const user = await userResponse.json();
-
-      const commentsResponse = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`
-      );
-      const comments = await commentsResponse.json();
+      const user = post.user;
+      const comments = post.comments;
 
       const listItem = document.createElement("li");
       listItem.innerHTML = `<a href="post.html?postId=${post.id}">${post.title} (${comments.length})</a> - <a href="user.html?userId=${user.id}">${user.name}</a>`;
--- user.js
+++ user.js
@@ -8,15 +8,12 @@ const userId = urlParams.get("userId");
 async function fetchUserPosts() {
   try {
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId}&_embed=comments`
     );
     const posts = await response.json();
 
     for (const post of posts) {
-      const commentsResponse = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`
-      );
-      const comments = await commentsResponse.json();
+      const comments = post.comments;
 
       const listItem = document.createElement("li");
       listItem.innerHTML = `<a href="post.html?postId=${post.id}">${post.title}</a> (${comments.length})`;
@@ -32,15 +29,13 @@ async function fetchUserPosts() {
 async function fetchUserAlbums() {
   try {
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+      `https://jsonplaceholder.typicode.com/albums?userId=${userId}&_embed=photos`
     );
     const albums = await response.json();
+    console.log(albums);
 
     for (const album of albums) {
-      const photosResponse = await fetch(
-        `https://jsonplaceholder.typicode.com/albums/${album.id}/photos`
-      );
-      const photos = await photosResponse.json();
+      const photos = album.photos;
 
       const listItem = document.createElement("li");
       listItem.innerHTML = `<a href="album.html?albumId=${album.id}">${album.title}</a> (${photos.length})`;
